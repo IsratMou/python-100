bc892b4f2b8ec20716df660c512dc227a4ecf11c
IsratMou <moutherobogirl@gmail.com>
2025-06-22 02:05:20 +0600

    matlibs game

---
+++ madlibs game.py
@@ -0,0 +1,15 @@
+# madlibs game
+# word game where you create a story
+# by filling in the blanks with words
+
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+adjective2 = input("Enter an adjective: ")
+verb1 = input("Enter a verb: ")
+adjective3 = input("Enter an adjective: ")
+
+# ask user for input
+print(f"today i went to a {adjective1} zoo.")
+print(f"In an exhibit, i saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"i was {adjective3}!")
