f940bb8b4f6739e0405ab738c427e051b4b9c248
IsratMou <moutherobogirl@gmail.com>
2025-07-25 16:11:21 +0600

    1up

---
+++ loop basic prob/prb1.py
@@ -0,0 +1,19 @@
+'''
+1. Print numbers 1 to 10
+python
+for num in range(1, 11):  # range(1,11) = 1 to 10
+    print(num)
+Explanation:
+
+range(start, stop) generates numbers from start to stop-1 (11 is excluded).
+
+num takes each value in the range.
+
+Alternative (while loop):
+
+python
+num = 1
+while num <= 10:
+    print(num)
+    num += 1  # Increment to avoid infinite loop
+'''
